5cc492c567a018748733a834f82e0ea7fd0bfcf8
danil <dani2201@mail.ru>
2023-11-20 15:55:23 +0600

    Examples012

--- Examples012_Methods/Program.cs
+++ Examples012_Methods/Program.cs
@@ -1,9 +1,9 @@
 ﻿// Вид 1
-void Method1()
-{
-    Console.WriteLine("Автор ...");
-}
-Method1();
+//void Method1()
+//{
+//Console.WriteLine("Автор ...");
+//}
+//Method1();
 
 
 
@@ -16,23 +16,23 @@ Method1();
 
 
 // Вид 2
-void Method2(string msg)
-{
-    Console.WriteLine(msg);
-}
-Method2(msg: "Текст сообщения");
+//void Method2(string msg)
+//{
+//Console.WriteLine(msg);
+//}
+//Method2(msg: "Текст сообщения");
 
-void Method21(string msg, int count)
-{
-    int i = 0;
-    while (i < count)
-    {
-        Console.WriteLine(msg);
-        i++;
-    }
-}
+//void Method21(string msg, int count)
+//{
+//int i = 0;
+// while (i < count)
+//{
+//Console.WriteLine(msg);
+//    i++;
+//  }
+//}
 //Method21(msg: "Текст", count: 4);
-Method21(count: 4, msg: "новый текст");
+//Method21(count: 4, msg: "новый текст");
 
 
 
@@ -42,13 +42,13 @@ Method21(count: 4, msg: "новый текст");
 
 // Вид 3
 
-int Method3()
-{
-    return DateTime.Now.Year;
-}
+//int Method3()
+//{
+//    return DateTime.Now.Year;
+//}
 
-int year = Method3();
-Console.WriteLine(year);
+//int year = Method3();
+//Console.WriteLine(year);
 
 
 
@@ -82,18 +82,69 @@ Console.WriteLine(year);
 
 // Вид 5
 
-string Method5(int count, string text)
+//string Method5(int count, string text)
+//{
+//string result = String.Empty;
+//for (int i = 0; i < count; i++)
+//{
+//    result = result + text;
+// }
+// return result;
+//}
+
+//string res = Method5(10, "q");
+//Console.WriteLine(res);
+
+
+//for (int i = 2; i <= 10; i++)
+//{
+//   for (int j = 2; j <= 10; j++)
+//   {
+//Console.WriteLine($"{i} * {j} = {i * j}");
+// }
+//Console.WriteLine();
+//}
+
+
+
+//===== Работа с текстом
+// Дан текст. В тексте нужно все пробелы заменить черточками,
+// маленькие буквы заменить "к" заменить большими "К",
+// а большие "С" заменить маленькими "с".
+
+// Ясна ли задача?
+
+internal class Program
 {
-    string result = String.Empty;
-    for (int i = 0; i < count; i++)
+    private static void Main(string[] args)
     {
-        result = result + text;
+        string text = "Я думаю, - сказал князь, улыбаясь, - что,"
+            + "ежели бы вас послали вместо нашего милого Винценгероде,"
+            + "вы бы взяли приступом согласие русского короля."
+            + "Вы так красноречивы. Вы дадите мне чаю?";
+
+
+        // string s = "qwerty"
+        //             0123
+        // s[3] // r
+
+        string Replase(string text, char oldValue, char newValue)
+        {
+            string result = String.Empty;
+            int length = text.length;
+            for (int i = 0; i < length; i++)
+            {
+                if (text[i] == oldValue) result = result + $"{newValue}";
+                else result = result + $"{text[i]}";
+            }
+
+            return result;
+        }
+
+        string newText = Replase(text, ' ', '|');
+
+        Console.WriteLine(newText);
+        Console.WriteLine();
+        newText = Replase(newText, 'k', 'K');
     }
-    return result;
 }
-
-string res = Method5(10, "q");
-Console.WriteLine(res);
-
-
-
